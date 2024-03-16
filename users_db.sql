7c23546c225cf09a813cfdf3462f28baef8255de
ryanteooooo <ryan.teo.2022@scis.smu.edu.sg>
2024-03-16 15:43:06 +0800

    c

--- users_db.sql
+++ users_db.sql
@@ -6,15 +6,16 @@ USE users_db;
 
 -- Create a table to store user data with the new table name
 CREATE TABLE IF NOT EXISTS users_fav_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     favourite VARCHAR(255) NOT NULL
+    CONSTRAINT pk_users_fav PRIMARY KEY (username, email)
+
 );
 
 -- Create a table to store user information (username and email)
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
+    CONSTRAINT pk_users PRIMARY KEY (username, email)
 );
