5e8814c18a0d45f2e5ea16b2267483a86fa6dc88
ryanteooooo <ryan.teo.2022@scis.smu.edu.sg>
2024-03-20 22:45:32 +0800

    change to my own 3306

--- carpark.py
+++ carpark.py
@@ -8,7 +8,7 @@ import logging
 app = Flask(__name__)
 CORS(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/carpark?charset=utf8'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:8889/carpark'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/carpark'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
