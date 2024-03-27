15752bddc8e856b121918c1efedb9332744809e8
ryanteooooo <ryan.teo.2022@scis.smu.edu.sg>
2024-03-27 10:12:37 +0800

    deleted preload db

--- preload_db/users_db.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users_fav_table (username, email, phone_number, favourite)
-VALUES 
-    ('1', '1@gmail.com', '+6596521596', 'A0004'),
-    ('2', '2@gmail.com', '+6587534490', 'A0004'),
-    ('1', '1@gmail.com', '+6596521596', 'A0005'),
-    ('2', '2@gmail.com', '+6587534490', 'A0005'),
-    ('1', '1@gmail.com', '+6596521596', 'A0006'),
-    ('1', '1@gmail.com', '+6596521596', 'A0007');
